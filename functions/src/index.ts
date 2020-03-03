1cd9712983983ba4b36ff85000b9719f12554cf8
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2020-03-03 10:27:41 +0100

    Set up Firebase project

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from 'firebase-functions';
+import {dialogflow, Suggestions} from 'actions-on-google';
+
+// Instantiate DialogFlow client
+interface ItemData {
+    item: object|undefined|string
+}
+const app = dialogflow<ItemData, {}>({debug: true});
+
+app.intent('out of items', (conv, {item}) => {
+    //Store item in user data?
+    conv.data.item = item;
+    conv.ask(`Would you like to add ${item} to your shopping list?`);
+    conv.ask(new Suggestions('Yes', 'No'));
+});
+
+app.intent('discounts - yes', (conv) => {
+    //Retrieve item from user data?
+    conv.close(`Sorry, no discounts available on ${conv.data.item}!`);
+});
+
+// Handle HTTPS POST requests
+export const fulfillment = functions.https.onRequest(app);
